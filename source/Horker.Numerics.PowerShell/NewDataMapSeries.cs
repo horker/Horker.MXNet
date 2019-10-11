684fa391c4edc2702eaf6bd7ea7bb6f425e7bccb
horker <horker@users.noreply.github.com>
2019-10-11 20:05:40 +0900

    Add New-DataMapSeries

---
+++ source/Horker.Numerics.PowerShell/NewDataMapSeries.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Horker.Numerics.DataMaps;
+
+namespace Horker.Numerics.PowerShell
+{
+    [Cmdlet("New", "DataMapSeries")]
+    [OutputType(typeof(Series))]
+    public class NewDataMapSeries : PSCmdlet
+    {
+        [Parameter(Position = 0, Mandatory = true, ParameterSetName = "New")]
+        public Type DataType;
+
+        [Parameter(Position = 1, Mandatory = true, ParameterSetName = "New")]
+        public int Count;
+
+        [Parameter(Position = 2, Mandatory = false, ParameterSetName = "New")]
+        public object Value = null;
+
+        [Parameter(Position = 0, Mandatory = true, ParameterSetName = "FromArray")]
+        public IList Data;
+
+        protected override void BeginProcessing()
+        {
+            if (ParameterSetName == "New")
+                WriteObject(new Series(DataType, Count, Value));
+            else
+                WriteObject(new Series(Data));
+        }
+    }
+}
--- source/Horker.Numerics/DataMaps/Series.cs
+++ source/Horker.Numerics/DataMaps/Series.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,6 +17,41 @@ namespace Horker.Numerics.DataMaps
         public Series(IList underlying)
         {
             _underlying = underlying;
+        }
+
+        private static IList CreateList<T>(int size)
+        {
+            var list = new List<T>(size);
+            for (var i = 0; i < size; ++i)
+                list.Add(default(T));
+
+            return list;
+        }
+
+        private static IList CreateListWithValue<T>(int size, T value)
+        {
+            var list = new List<T>(size);
+            for (var i = 0; i < size; ++i)
+                list.Add(value);
+
+            return list;
+        }
+
+        public Series(Type dataType, int size, object value = null)
+        {
+            if (value == null)
+            {
+                var m = typeof(Series).GetMethod("CreateList", BindingFlags.NonPublic | BindingFlags.Static);
+                var gm = m.MakeGenericMethod(new Type[] { dataType });
+                _underlying = (IList)gm.Invoke(null, new object[] { size });
+
+            }
+            else
+            {
+                var m = typeof(Series).GetMethod("CreateListWithValue", BindingFlags.NonPublic | BindingFlags.Static);
+                var gm = m.MakeGenericMethod(new Type[] { dataType });
+                _underlying = (IList)gm.Invoke(null, new object[] { size, value });
+            }
         }
     }
 }
