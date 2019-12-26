fc3a68295cf8f89962e608a27abfa29fb89acc72
horker <horker@users.noreply.github.com>
2019-12-26 20:07:43 +0900

    Define Utilities.LibSVMFormatLoader

---
+++ source/Horker.Numerics.Tests/LibSVMFormatLoaderTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Horker.Numerics.Utilities;
+using Xunit;
+
+namespace Horker.Numerics.Tests
+{
+    public class LibSVMFormatLoaderTest
+    {
+        [Fact]
+        public void TestLoad()
+        {
+            var text = @"1 1:10 2:20 3:30
+-1 2:22 9:99";
+
+            double[] labels;
+            double[,] data;
+            using (var s = new StreamReader(new MemoryStream(Encoding.UTF8.GetBytes(text)), Encoding.UTF8))
+            {
+                (labels, data) = LibSVMFormatLoader.Load<double>(s);
+            }
+
+            Assert.Equal(new double[] { 1, -1 }, labels);
+
+            Assert.Equal(2, data.GetLength(0));
+            Assert.Equal(9, data.GetLength(1));
+            Assert.Equal(10.0, data[0, 0]);
+            Assert.Equal(20.0, data[0, 1]);
+            Assert.Equal(30.0, data[0, 2]);
+            Assert.Equal(0.0, data[0, 8]);
+
+            Assert.Equal(0.0, data[1, 0]);
+            Assert.Equal(22.0, data[1, 1]);
+            Assert.Equal(0.0, data[1, 4]);
+            Assert.Equal(99.0, data[1, 8]);
+        }
+    }
+}
---
+++ source/Horker.Numerics/Utilities/LibSVMFormatLoader.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Horker.Numerics.Utilities
+{
+    public static class LibSVMFormatLoader
+    {
+        public static Tuple<T[], T[,]> Load<T>(TextReader reader)
+        {
+            var lines = new List<string[]>();
+
+            var split = new[] { ' ', '\t', ',' };
+            while (reader.Peek() > -1)
+            {
+                var line = reader.ReadLine().Split(split);
+                lines.Add(line);
+            }
+
+            var maxIndex = 0;
+            var cellSplit = new[] { ':' };
+            foreach (var line in lines)
+            {
+                var last = line[line.Length - 1];
+                var tuple = last.Split(cellSplit);
+                if (tuple.Length != 2)
+                    throw new ArgumentException("bad format");
+                var index = int.Parse(tuple[0]);
+                if (index > maxIndex)
+                    maxIndex = index;
+            }
+
+            var labels = new T[lines.Count];
+            var data = new T[lines.Count, maxIndex];
+
+            for (var row = 0; row < lines.Count; ++row)
+            {
+                var line = lines[row];
+
+                var label = double.Parse(line[0]);
+                labels[row] = (T)(object)label;
+
+                for (var column = 1; column < line.Length; ++column)
+                {
+                    var cell = line[column];
+                    var tuple = cell.Split(cellSplit);
+                    if (tuple.Length != 2)
+                        throw new ArgumentException("bad format");
+                    var index = int.Parse(tuple[0]);
+                    var value = double.Parse(tuple[1]);
+
+                    data[row, index - 1] = (T)(object)value;
+                }
+            }
+
+            return Tuple.Create(labels, data);
+        }
+    }
+}
